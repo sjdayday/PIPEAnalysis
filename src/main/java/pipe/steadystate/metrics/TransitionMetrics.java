54d75f613682875e5a093e9fc90b43f70f10d98e
Steve Doubleday <stevedoubleday@gmail.com>
2016-08-28 12:41:14 -0700

    hierarchical-nets compatibility

--- src/main/java/pipe/steadystate/metrics/TransitionMetrics.java
+++ src/main/java/pipe/steadystate/metrics/TransitionMetrics.java
@@ -2,6 +2,7 @@ package pipe.steadystate.metrics;
 
 import uk.ac.imperial.pipe.animation.AnimationLogic;
 import uk.ac.imperial.pipe.animation.PetriNetAnimationLogic;
+import uk.ac.imperial.pipe.models.petrinet.ExecutablePetriNet;
 import uk.ac.imperial.pipe.models.petrinet.PetriNet;
 import uk.ac.imperial.pipe.models.petrinet.Transition;
 import uk.ac.imperial.state.ClassifiedState;
@@ -21,7 +22,8 @@ public final class TransitionMetrics {
     private TransitionMetrics() {}
 
     public static Map<String, Double> getTransitionThroughput(Map<Integer, ClassifiedState> stateSpace, Map<Integer, Double> steadyState, PetriNet petriNet) {
-        AnimationLogic animationLogic = new PetriNetAnimationLogic(petriNet);
+    	ExecutablePetriNet executablePetriNet = petriNet.getExecutablePetriNet(); 
+    	AnimationLogic animationLogic = new PetriNetAnimationLogic(executablePetriNet);
         Map<String, Double> throughputs = new HashMap<>();
         for (Map.Entry<Integer, ClassifiedState> entry : stateSpace.entrySet()) {
             int id = entry.getKey();
@@ -29,7 +31,7 @@ public final class TransitionMetrics {
 
             for (Transition transition : animationLogic.getEnabledTransitions(state)) {
                 String transitionId = transition.getId();
-                double throughput = transition.getActualRate(petriNet, state) * steadyState.get(id);
+                double throughput = transition.getActualRate(executablePetriNet) * steadyState.get(id);
                 double previous = throughputs.containsKey(transitionId) ? throughputs.get(transitionId) : 0;
                 throughputs.put(transitionId, throughput + previous);
             }
